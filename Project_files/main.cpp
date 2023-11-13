65b2853d07b1a5d58679eb950efcb74ba55d2b19
Hexagonist <januszkrem2@wp.pl>
2023-11-13 03:02:01 +0100

    Button header and very basic button

--- Project_files/main.cpp
+++ Project_files/main.cpp
@@ -1,21 +1,48 @@
 #include <SFML/Graphics.hpp>
 
+#include "Header Files\Button.h"
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    // window 
+    // to implement:    (sf::VideoMode::getDesktopMode().width / 2), (sf::VideoMode::getDesktopMode().height / 2)
+
+    sf::Vector2i centerWindow((sf::VideoMode::getDesktopMode().width / 2) - 445, (sf::VideoMode::getDesktopMode().height / 2) - 480);
+
+    sf::RenderWindow window(sf::VideoMode(900, 900),  "War of Age Game");
+    window.setPosition(centerWindow);
+
+    window.setKeyRepeatEnabled(true);
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    Button btn1("Start", {200, 50}, 20, sf::Color::Red, sf::Color::Black);
+    btn1.setPosition({100, 300});
+    //btn1.setFont(arial);
+
+    // Main loop
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            // event closing window after Event::Closed triggered
+            if (event.type == sf::Event::Closed)    window.close();
+
+            // button 1 's reactions to mouse
+            if (event.type == sf::Event::MouseMoved) {
+                if (btn1.isMouseOver(window)) {
+                    btn1.setBackColor(sf::Color::White);
+                }
+                else {
+                    btn1.setBackColor(sf::Color::Red);
+                }
+            }
         }
 
         window.clear();
+        btn1.drawTo(window);
         window.draw(shape);
         window.display();
     }
